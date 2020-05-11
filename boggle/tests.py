d7f387aca580d69cec1ad159ed4d326dcb76d224
alexpilk <alex.pilkevych@gmail.com>
2020-05-11 14:35:39 +0200

    [R] more test cases for different words

--- boggle/tests.py
+++ boggle/tests.py
@@ -11,11 +11,11 @@ def board():
     ])
 
 
-@pytest.mark.parametrize('word', ('ABCCED', 'SEE'))
+@pytest.mark.parametrize('word', ('ABCCED', 'SEE', 'SAB', 'CEESEC'))
 def test_correct_word(board, word):
     assert board.check_word(word) is True
 
 
-@pytest.mark.parametrize('word', ('ABCB',))
+@pytest.mark.parametrize('word', ('ABCB', 'SCCC', 'CEESECC'))
 def test_incorrect_word(board, word):
     assert board.check_word(word) is False
