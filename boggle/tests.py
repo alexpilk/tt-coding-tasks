c7f1844d45cddc2f95f40eafde2b429baa417094
alexpilk <alex.pilkevych@gmail.com>
2020-05-11 13:27:00 +0200

    [R] added test for simple words on Board

---
+++ boggle/tests.py
@@ -0,0 +1,16 @@
+import pytest
+from boggle import Board
+
+
+@pytest.fixture
+def board():
+    return Board([
+        ['A', 'B', 'C', 'E'],
+        ['S', 'F', 'C', 'S'],
+        ['A', 'D', 'E', 'E']
+    ])
+
+
+@pytest.mark.parametrize('word', ('ABCCED', 'SEE'))
+def test_correct_word(board, word):
+    assert board.check_word(word) is True
