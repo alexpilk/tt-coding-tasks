6bb4fadb4122315a3b70e473a9c8a4f4460ab06a
alexpilk <alex.pilkevych@gmail.com>
2020-05-11 15:11:21 +0200

    [B] refactor

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -1,45 +1,77 @@
+from typing import List, Tuple
+
+
 class Board:
 
-    def __init__(self, board):
+    def __init__(self, board: List[List[str]]):
         self.board = board
 
-    def check_word(self, word):
-        starting_points = self._find_starting_points(word[0])
-        print(word[0], starting_points)
-        if not starting_points:
-            return False
-        return self._process(list(word[1:]), starting_points)
+    def check_word(self, word: str) -> bool:
+        """
+        Checks whether the word is present on board.
+        """
+        starting_cells = self._find_starting_cells(word[0])
 
-    def _process(self, word, starting_points, forbidden=None):
+        return self._search(list(word[1:]), starting_cells)
+
+    def _search(self, word: List[str], starting_cells: List[Tuple[int]], forbidden: List[Tuple[int]] = None) -> bool:
+        """
+        Recursively searches the board for next letters in the word.
+
+        :param word: e.g. 'SEE'
+        :param starting_cells: e.g. [(1, 0), (3, 2)]
+        :param forbidden: cells already included in word that should not be revisited
+        """
         forbidden = forbidden or []
 
         if not word:
             return True
 
         next_letter = word.pop(0)
-        for point in starting_points:
-            new_forbidden = forbidden + [point]
-            new_starting_points = self._find_next_letters(point, next_letter, forbidden=new_forbidden)
-            print(next_letter, new_starting_points)
-            if not new_starting_points:
+
+        for cell in starting_cells:
+            new_forbidden = forbidden + [cell]
+            new_starting_cells = self._find_possible_cells(cell, next_letter, forbidden=new_forbidden)
+
+            if not new_starting_cells:
                 continue
-            if self._process(list(word), new_starting_points, new_forbidden):
+
+            if self._search(list(word), new_starting_cells, new_forbidden):
                 return True
 
         return False
 
-    def _find_starting_points(self, letter):
-        coordinates = []
+    def _find_starting_cells(self, letter: str) -> List[Tuple[int]]:
+        """
+        Finds all cells containing starting letter of the word.
+        """
+        cells = []
         for row_index, row in enumerate(self.board):
             if letter in row:
-                coordinates += [(row_index, i) for i, val in enumerate(row) if val == letter]
-        return coordinates
+                cells += [(row_index, i) for i, val in enumerate(row) if val == letter]
+        return cells
 
-    def _find_next_letters(self, coordinates, letter, forbidden):
-        possible_cells = [cell for cell in self._adjacent_cells(coordinates) if cell not in forbidden]
-        return [cell for cell in possible_cells if self.board[cell[0]][cell[1]] == letter]
+    def _find_possible_cells(self, cell: Tuple[int], letter: str, forbidden: List[Tuple[int]]):
+        """
+        Checks given cell for adjacent cells containing given letter.
 
-    def _adjacent_cells(self, coordinates):
+        :param cell: e.g. (1, 0)
+        :param letter: e.g. 'S'
+        :param forbidden: cells already included in word that should not be revisited
+        :return: cells that are possible continuations of the route
+        """
+        adjacent_cells = self._adjacent_cells(cell)
+        return [
+            _cell for _cell in adjacent_cells if _cell not in forbidden and self._get_letter(_cell) == letter
+        ]
+
+    def _get_letter(self, cell: Tuple[int]) -> str:
+        return self.board[cell[0]][cell[1]]
+
+    def _adjacent_cells(self, cell: Tuple[int]) -> List[Tuple[int]]:
+        """
+        Returns coordinates of all adjacent cells.
+        """
         offsets = [
             (0, 1),
             (0, -1),
@@ -47,9 +79,12 @@ class Board:
             (-1, 0)
         ]
         return [
-            (coordinates[0] + i, coordinates[1] + j) for i, j in offsets if
-            self._check_coordinates((coordinates[0] + i, coordinates[1] + j))
+            result for i, j in offsets if
+            self._check_coordinates(result := (cell[0] + i, cell[1] + j))
         ]
 
-    def _check_coordinates(self, coordinates):
-        return 0 <= coordinates[0] < len(self.board) and 0 <= coordinates[1] < len(self.board[0])
+    def _check_coordinates(self, cell: Tuple[int]) -> bool:
+        """
+        Checks if cell is within board dimensions.
+        """
+        return 0 <= cell[0] < len(self.board) and 0 <= cell[1] < len(self.board[0])
