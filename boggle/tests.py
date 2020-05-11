ac854cda5fa29dd5bad9f164b2da03a8057f434f
alexpilk <alex.pilkevych@gmail.com>
2020-05-11 13:29:03 +0200

    [R] added test for incorrect words

--- boggle/tests.py
+++ boggle/tests.py
@@ -14,3 +14,8 @@ def board():
 @pytest.mark.parametrize('word', ('ABCCED', 'SEE'))
 def test_correct_word(board, word):
     assert board.check_word(word) is True
+
+
+@pytest.mark.parametrize('word', ('ABCB',))
+def test_incorrect_word(board, word):
+    assert board.check_word(word) is False
